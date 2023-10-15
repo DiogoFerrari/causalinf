07831702472daaef8092050f26e6e0aa3ecf080a
diogoferrari <diogoferrari@gmail.com>
2023-10-15 15:43:40 -0700

    Attempt

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 #Location of Sphinx files
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'causalinf'
 copyright = '2023, Diogo Ferrari'
