a53e3f43534081d5ea898e325ed43e7326d4e7fc
diogoferrari <diogoferrari@gmail.com>
2026-06-04 15:35:06 -0700

    ready to deploy website updated

--- site/src/constants.py
+++ site/src/constants.py
@@ -4,7 +4,6 @@
 #+MACRO: PROJECT_NAME Causal Inference (causalinf)
 #+MACRO: SITE_URL https://www.diogoferrari.com/causalinf
 #+MACRO: AUTHOR Diogo Ferrari
-FERRARI_BOOK = "Ferrari, D. (2026). The Identification of Causal Effects. Cambridge Universty Press."
-
+FERRARI_BOOK = "Ferrari, D. (forthcoming). The Identification of Causal Effects. Cambridge University Press."
 
 
